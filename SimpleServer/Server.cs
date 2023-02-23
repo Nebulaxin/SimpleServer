a4f824c3d077662a245c8c66d40bd14ca4353179
Nebulaxin <krivts0663@yandex.ru>
2023-02-23 08:29:27 +0400

    Small changes

--- SimpleServer/Server.cs
+++ SimpleServer/Server.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
@@ -9,26 +8,54 @@ namespace SimpleServer
     {
         public HttpListener Listener { get; private set; }
 
-        public Server(string host)
+        private bool running;
+
+        public Server(string url)
         {
             Listener = new();
-            Listener.Prefixes.Add(host);
+            Listener.Prefixes.Add(url);
         }
 
-        public async Task Start()
+        /// <summary>
+        /// Starts recieving requests
+        /// </summary>
+        public void Start()
         {
+            if (running) return;
+            running = true;
             Listener.Start();
-            while (true)
-            {
-                Console.WriteLine("waiting");
-                var context = await Listener.GetContextAsync();
+            Task.Factory.StartNew(Listen, TaskCreationOptions.LongRunning);
+        }
 
-                Console.WriteLine("responding");
+        private async Task Listen()
+        {
+            while (running)
+            {
+                HttpListenerContext context;
+                try
+                {
+                    context = await Listener.GetContextAsync();
+                }
+                catch (HttpListenerException)
+                {
+                    return;
+                }
+                if (!running) return;
+                var request = context.Request;
                 using var response = context.Response;
                 using var writer = new StreamWriter(response.OutputStream);
                 await writer.WriteAsync("<html><body><h1>This is default response</h1></body></html>");
-                Console.WriteLine("responded");
             }
+        }
+
+        /// <summary>
+        /// Stops recieving requests
+        /// </summary>
+        public void Stop()
+        {
+            if (!running) return;
+            running = false;
+            Listener.Stop();
         }
     }
 }
--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,8 +1,15 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text.Json.Nodes;
+using System.Threading.Tasks;
 using SimpleServer;
 
-var config = JsonNode.Parse(await File.ReadAllTextAsync("~/config.json"));
+var config = JsonNode.Parse(await File.ReadAllTextAsync("config.json"));
 var server = new Server((string)config["host"]);
 
-await server.Start();
+bool running = true;
+Console.CancelKeyPress += (o, e) => running = !(e.Cancel = true);
+
+server.Start();
+while (running) Console.ReadKey(true);
+server.Stop();
